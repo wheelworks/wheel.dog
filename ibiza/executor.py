e45217d3eb0d867a3c31afe54dd60b8b91332858
Squeaky <squeaky_pl@gmx.com>
2016-04-27 23:43:21 +0200

    use socat to proxy to docker and make simple executor

--- ibiza/executor.py
+++ ibiza/executor.py
@@ -1,46 +1,23 @@
-import os
 import asyncio as aio
+import os
 import pprint
+import sys
 
 import aiohttp
 
 
-async def start_socat():
-    return await aio.create_subprocess_exec(
-        'socat', '-d', '-d', 'TCP-LISTEN:8080,fork', 'UNIX:/var/run/docker.sock')
-
-
-def drop_privileges():
-    os.setgroups([])
-
-    os.setegid(10000)
-    os.seteuid(10000)
-
-
-async def query_containers(proxy):
-    async with proxy.get('http://localhost:8080/containers/json') as response:
+async def query_containers(proxy_client):
+    async with proxy_client.get('http://socat:8080/containers/json') as response:
         pprint.pprint(await response.json())
 
 
 def main():
     loop = aio.get_event_loop()
 
-    proxy = aiohttp.ClientSession()
+    proxy_client = aiohttp.ClientSession()
 
-    socat = loop.run_until_complete(start_socat())
-    print('Started TCP to UNIX socket proxy')
-
-    drop_privileges()
-    print('Dropped root privileges')
-
-    loop.run_until_complete(query_containers(proxy))
-
-    loop.run_until_complete(proxy.close())
-
-    print('Gained root priviledges')
-
-    socat.terminate()
-    loop.run_until_complete(socat.wait())
+    loop.run_until_complete(query_containers(proxy_client))
+    loop.run_until_complete(proxy_client.close())
 
     loop.close()
 
